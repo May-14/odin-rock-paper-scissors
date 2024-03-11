a11f1188f9c37aa31aba8990204b1e297434a768
May <26matmoran@gmail.com>
2024-03-10 20:43:23 -0600

    Add function to decide winner of round

--- script.js
+++ script.js
@@ -2,12 +2,33 @@ function getComputerChoice() {
     let randomNumber = Math.floor(Math.random() * 3);
     switch (randomNumber) {
         case 0:
-            return "Rock"
+            return "Rock";
         case 1:
-            return "Scissors"
+            return "Scissors";
         case 2:
-            return "Paper"
+            return "Paper";
     }
 }
 
-console.log(getComputerChoice())
+function playRound(playerChoice, computerChoice) {
+    if (playerChoice === computerChoice) {
+        return `Tie. You both chose ${computerChoice}.`
+    } else if (((playerChoice === "Rock") & computerChoice === "Paper") || ((playerChoice === "Paper") & computerChoice === "Scissors") || ((playerChoice === "Scissors") & computerChoice === "Rock")) {
+        return `You lose! ${computerChoice} beats ${playerChoice.toLowerCase()}.`
+    } else {
+        return `You win! ${playerChoice} beats ${computerChoice.toLowerCase()}.`
+    }
+}
+
+function playGame() {
+    for (let i = 0; i < 5; i++){
+        alert(i)
+    }
+}
+
+playGame()
+// let playerChoice = prompt("Enter something");
+// playerChoice = playerChoice[0].toUpperCase() + playerChoice.slice(1).toLowerCase()
+// let computerChoice = getComputerChoice();
+// alert(computerChoice)
+// alert(playRound(playerChoice, computerChoice))
