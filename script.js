74ccdee2d48efe4ffa3e3f504ca796531bce5e98
May <26matmoran@gmail.com>
2024-03-10 20:23:16 -0600

    Add function to get computer's choice

---
+++ script.js
@@ -0,0 +1,13 @@
+function getComputerChoice() {
+    let randomNumber = Math.floor(Math.random() * 3);
+    switch (randomNumber) {
+        case 0:
+            return "Rock"
+        case 1:
+            return "Scissors"
+        case 2:
+            return "Paper"
+    }
+}
+
+console.log(getComputerChoice())
