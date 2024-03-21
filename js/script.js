a3fba44f750b189f4ca456f1afd1d5767c7d12e0
May <26matmoran@gmail.com>
2024-03-20 18:27:25 -0600

    Change logic of playRound and playGame functions
    Before this change, the playGame function was needed to play
    the game; you couldn't play one round by calling the playRound
    function. It simply did not work. Change the logic around so all things
    happen inside the playRound function, and the playGame func just
    repeats the playRound func and gives the end result. Still, the playGame
    func for now, is commented out.

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+let playerRoundScore = 0;
+let computerRoundScore = 0;
 function getComputerChoice() {
     let randomNumber = Math.floor(Math.random() * 3);
     switch (randomNumber) {
@@ -10,46 +12,53 @@ function getComputerChoice() {
     }
 }
 
-function playRound(playerChoice, computerChoice) {
+function playRound(roundNumber) {
+    let playerChoice = prompt("Enter something");
+    playerChoice = playerChoice[0].toUpperCase() +  playerChoice.slice(1).toLowerCase()
+    let computerChoice = getComputerChoice();
+    let roundResult = "";
     if (playerChoice === computerChoice) {
-        return "Tie";
+        roundResult = "Tie";
     } else if (((playerChoice === "Rock") & computerChoice === "Paper") || ((playerChoice === "Paper") & computerChoice === "Scissors") || ((playerChoice === "Scissors") & computerChoice === "Rock")) {
-        return "Lose";
+        roundResult = "Lose";
     } else if (playerChoice !== "Rock" && playerChoice !== "Scissors" && playerChoice !== "Paper") {
         alert("Please enter a valid input. Rock, paper or scissors. (Not case sensitive)")
     } else {
-        return "Win";
+        roundResult = "Win";
+    }
+    switch (roundResult) {
+        case "Win":
+            alert(`You win round number ${roundNumber + 1}! ${playerChoice} beats ${computerChoice.toLowerCase()}`);
+            playerRoundScore += 1;
+            break;
+        case "Lose":
+            alert(`You lose round number ${roundNumber + 1}! ${computerChoice} beats ${playerChoice.toLowerCase()}`);
+            computerRoundScore += 1;
+            break;
+        case "Tie":
+            alert(`Round ${roundNumber + 1} ends in tie! You both chose ${computerChoice}`);
+            break;
+        default:
+            i -= 1;
     }
 }
 
-// function playGame() {
-//     let playerRoundScore = 0;
-//     let computerRoundScore = 0;
-//     for (let i = 0; i < 5; i++){
-//         let playerChoice = prompt("Enter something");
-//         playerChoice = playerChoice[0].toUpperCase() +  playerChoice.slice(1).toLowerCase()
-//         let computerChoice = getComputerChoice();
-//         let roundResult = playRound(playerChoice, computerChoice);
-//         switch (roundResult) {
-//             case "Win":
-//                 alert(`You win round number ${i + 1}! ${playerChoice} beats ${computerChoice.toLowerCase()}`);
-//                 playerRoundScore += 1;
-//                 break;
-//             case "Lose":
-//                 alert(`You lose round number ${i + 1}! ${computerChoice} beats ${playerChoice.toLowerCase()}`);
-//                 computerRoundScore += 1;
-//                 break;
-//             case "Tie":
-//                 alert(`Round ${i + 1} ends in tie! You both chose ${computerChoice}`);
-//                 break;
-//             default:
-//                 i -= 1;
-//         }
-//     }
-//     if (playerRoundScore === computerRoundScore) {
-//         return(`You tied with a score of ${playerRoundScore}.`);
-//     } else {
-//         return((`You ${computerRoundScore > playerRoundScore ? "lost" : "won"} with a score of ${playerRoundScore}-${computerRoundScore}.`));
-//     }
-// }
+let buttons = document.querySelectorAll("button");
+buttons.forEach(button => {
+    button.addEventListener("click", playRound())
+})
+function playGame() {
+    playerRoundScore = 0;
+    computerRoundScore = 0;
+    for (let i = 0; i < 5; i++){
+        playRound(i)
+    }
+    if (playerRoundScore === computerRoundScore) {
+        return(`You tied with a score of ${playerRoundScore}.`);
+    } else {
+        return((`You ${computerRoundScore > playerRoundScore ? "lost" : "won"} with a score of ${playerRoundScore}-${computerRoundScore}.`));
+    }
+}
+
+alert(playGame())
 
